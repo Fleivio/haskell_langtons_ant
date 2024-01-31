3a4788744a777df7857467c5803ed0f4b509b986
Anomalocaris <flavioborinjunior@gmail.com>
2024-01-31 19:27:36 -0300

    readme

--- ReadmeGen.hs
+++ ReadmeGen.hs
@@ -28,7 +28,7 @@ writeGroup gName imgs =
 
 writeImgCell :: String -> String
 writeImgCell path = 
-    "<img src=\"" ++ path ++ "\" width=\"400\" />"
+    "<img src=\"" ++ path ++ "\" max_width=\"400\" />"
     -- ++ "<span>"++ rule ++"</span>\n</div>\n"
     -- where
     --     rule = filter isUpper . reverse . takeWhile (/= '/') $ dropWhile (/= '.') $ reverse path
